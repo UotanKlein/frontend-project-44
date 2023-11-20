dc35c5159697aae735093d7e10947884125e1699
UotanKlein <francferdinant2@gmail.com>
2023-11-20 23:17:59 +0300

    Complete task 3

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+function getName() {
+    const name = readlineSync.question('May I have your name?: ');
+    console.log(`Hello, ${name}!`);
+}
+
+export { getName }
